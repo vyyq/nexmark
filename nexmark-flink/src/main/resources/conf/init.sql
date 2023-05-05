6d002cc19280617a66abeb8be939345b6efacda3
Yuqin Yan <35229879+vyyq@users.noreply.github.com>
2023-05-05 18:40:17 -0400

    Update init.sql

--- nexmark-flink/src/main/resources/conf/init.sql
+++ nexmark-flink/src/main/resources/conf/init.sql
@@ -3,12 +3,5 @@ SET 'sql-client.execution.result-mode' = 'table';
 SET 'pipeline.auto-watermark-interval' = '200';
 SET 'pipeline.time-characteristic' = 'EventTime';
 SET 'sql-client.execution.max-table-result.rows' = '1000000';
-SET 'parallelism.default' = '8';
 SET 'pipeline.max-parallelism' = '128';
 SET 'restart-strategy.type' = 'fixed-delay';
-
-SET 'table.exec.mini-batch.enabled' = 'true';
-SET 'table.exec.mini-batch.allow-latency' = '2s';
-SET 'table.exec.mini-batch.size' = '50000';
-SET 'table.optimizer.distinct-agg.split.enabled' = 'true';
-
