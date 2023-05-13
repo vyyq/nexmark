3d7e7c106759422cce26372fbe69f424fbdd2567
Yuqin Yan <yuqin.yan@mail.utoronto.ca>
2023-05-13 14:51:50 -0400

    Modify perf scripts

--- nexmark-flink/src/main/resources/bin/perf_start_on_single_node.sh
+++ nexmark-flink/src/main/resources/bin/perf_start_on_single_node.sh
@@ -1,22 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/env bash
 
-# bin=`dirname "$0"`
-# bin=`cd "$bin"; pwd`
-# 
-# . "$bin"/config.sh
-
-jvm_pid_list=$(jps | grep -i 'TaskManagerRunner' | awk '{print $1}')
-
-echo "jvm_pid_list: ${jvm_pid_list}"
-
-events="-e LLC-load-misses -e LLC-loads -e LLC-store-misses -e LLC-stores"
-
-for jvm_pid in ${jvm_pid_list}
-do
-    # perf on these JVM processes
-    echo "perf record $events -p $jvm_pid -o perf_${jvm_pid}.data -F 99 -m 2048 &"
-    perf record $events -p $jvm_pid -o perf_${jvm_pid}.data -F 99 -m 2048 &
-    sleep 0.5
-done
-
+echo "sudo perf mem record -a &"
+sudo perf mem record -a &
 echo "Perf start: DONE!"
+
